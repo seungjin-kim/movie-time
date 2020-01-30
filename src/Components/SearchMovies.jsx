52965da812b984094a06435f25f61bd3e18a3843
seungjin-kim <seungjink1@gmail.com>
2020-01-29 21:20:30 -0800

    Initialize app

---
+++ src/Components/SearchMovies.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default class SearchMovies extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hi: [],
+    };
+
+  }
+
+
+
+
+
+
+  render () {
+
+
+
+    return (
+      <>
+      </>
+    );
+  }
+}
