0b447733adf44ca6f93dd5e46cf2ab08729742c9
seungjin-kim <seungjink1@gmail.com>
2020-02-03 13:24:36 -0800

    Implement filterResult function and add axios

--- src/Components/SearchMovies.jsx
+++ src/Components/SearchMovies.jsx
@@ -3,10 +3,14 @@ import {
   Container,
   Button,
   Input,
+  InputGroup,
+  InputGroupAddon,
   Row, Col,
+  
 
 } from "reactstrap";
 import Movie from './Movie.jsx'
+import axios from 'axios'
 
 const exampleData = [
   {id: 0, title: "Avengers: Infinity War", overview: "blah blah blah 1"},
@@ -24,16 +28,55 @@ export default class SearchMovies extends React.Component {
   }
 
 
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  filterResult(data) {
+    return {
+      id: data.id,
+      poster: data.poster_path,
+      rating: data.vote_average,
+      title: data.name ? data.name : data.title,
+      released: data.release_date,
+      lang: data.original_language,
+      overview: data.overview,
+    }
+  }
+
+  getMovies() {
+    var options = {
+      key: this.props.API_KEY,
+    }
+
+
+  }
+
+
+
 
 
 
 
   render () {
 
-
-
     return (
       <Container>
+
+        <Row className="search">
+          <Col sm="12">
+            <InputGroup>
+              <Input placeholder="Find a movie"/>
+              <InputGroupAddon addonType="prepend">
+                <Button color="success"
+                  className="search-button">
+                    Search
+                </Button>
+              </InputGroupAddon>
+            </InputGroup>
+          </Col>  
+        </Row>
+
         <Row className="content">
           {this.state.movies.map(movie => 
             <Col
--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MOVIEDB_API_KEY from  './config/moviedb.js'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<App API_KEY={MOVIEDB_API_KEY} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
