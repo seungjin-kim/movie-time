88d4132a8611b42334a1ba2edd589862528e31e2
seungjin-kim <seungjink1@gmail.com>
2020-02-03 15:07:52 -0800

    Home page rendering full movie details

--- src/Components/Movie.jsx
+++ src/Components/Movie.jsx
@@ -15,12 +15,16 @@ const Movie = (props) => (
   <Container>
     <Row>
       <Col>
+        <img alt="poster" src={`https://image.tmdb.org/t/p/w200/${props.movie.poster}`} />
       </Col>
 
       <Col>
         <div className="movie-entry media">
           <div className="media-body">
-            <div>{props.movie.title}</div>
+            {props.movie.title}
+            <p> Rating: {props.movie.rating} </p>
+            <p> {props.movie.overview} </p>
+            <p> Release Date: {props.movie.released} </p>
           </div>
         </div>
       </Col>
